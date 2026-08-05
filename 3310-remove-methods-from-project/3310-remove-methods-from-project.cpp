89ca6a105896f19328f9ab4a522cf703fb280a2a
Jayant <jayantchoudhary2901@gmail.com>
2026-08-06 00:37:36 +0530

    Time: 164 ms (81.03%), Space: 298.7 MB (84.83%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> graph(n);
+
+        for (auto &edge : invocations) {
+            graph[edge[0]].push_back(edge[1]);
+        }
+
+        vector<bool> suspicious(n, false);
+        queue<int> q;
+
+        q.push(k);
+        suspicious[k] = true;
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+
+            for (int next : graph[curr]) {
+                if (!suspicious[next]) {
+                    suspicious[next] = true;
+                    q.push(next);
+                }
+            }
+        }
+
+        for (auto &edge : invocations) {
+            int from = edge[0];
+            int to = edge[1];
+
+            if (!suspicious[from] && suspicious[to]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++) {
+                    ans.push_back(i);
+                }
+                return ans;
+            }
+        }
+
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
